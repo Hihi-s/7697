3c1059e86bbd5927dbdb2b39496d3575d97e26b7
Hihi-s <55428253+Hihi-s@users.noreply.github.com>
2020-01-07 18:12:07 +0800

    Create FinalProject.ino

---
+++ FinalProject.ino
@@ -0,0 +1,333 @@
+#include "Wire.h"
+#include <LWiFi.h>
+#include <WiFiClient.h>
+/*
+  char ssid[] = "BuLi";
+  char pass[] = "dogisyou111";*/
+char ssid[] = "呵呵";
+char pass[] = "jessica77593";
+int keyIndex = 0;
+int status = WL_IDLE_STATUS;
+char server[] = "maker.ifttt.com";
+const int httpPort = 80;
+WiFiClient client;
+#include "I2Cdev.h"
+#include "MPU9250.h"
+MPU9250 accelgyro;
+I2Cdev   I2C_M;
+uint8_t buffer_m[6];
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+int16_t   mx, my, mz;
+float heading;
+float tiltheading;
+float Axyz[3];
+float Gxyz[3];
+float Mxyz[3];
+#define sample_num_mdate  5000
+volatile float mx_sample[3];
+volatile float my_sample[3];
+volatile float mz_sample[3];
+static float mx_centre = 0;
+static float my_centre = 0;
+static float mz_centre = 0;
+volatile int mx_max = 0;
+volatile int my_max = 0;
+volatile int mz_max = 0;
+volatile int mx_min = 0;
+volatile int my_min = 0;
+volatile int mz_min = 0;
+void setup() {
+  Wire.begin();
+  Serial.begin(38400);
+  Serial.println("Initializing I2C devices...");
+  accelgyro.initialize();
+  Serial.println("Testing device connections...");
+  Serial.println(accelgyro.testConnection() ? "MPU9250 connection successful" : "MPU9250 connection failed");
+  delay(1000);
+  Serial.println("     ");
+  while (!Serial) {
+    ; 
+  }
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+  }
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWifiData();
+}
+
+void loop()
+{
+  printCurrentNet();
+  getAccel_Data();
+  getGyro_Data();
+  getCompassDate_calibrated(); // compass data has been calibrated here
+  getHeading();
+  getTiltHeading();
+  Serial.println("calibration parameter: ");
+  Serial.print(mx_centre);
+  Serial.print("         ");
+  Serial.print(my_centre);
+  Serial.print("         ");
+  Serial.println(mz_centre);
+  Serial.println("     ");
+  Serial.println("Acceleration(g) of X,Y,Z:");
+  Serial.print(Axyz[0]);
+  Serial.print(",");
+  Serial.print(Axyz[1]);
+  Serial.print(",");
+  Serial.println(Axyz[2]);
+  Serial.println("Gyro(degress/s) of X,Y,Z:");
+  Serial.print(Gxyz[0]);
+  Serial.print(",");
+  Serial.print(Gxyz[1]);
+  Serial.print(",");
+  Serial.println(Gxyz[2]);
+  Serial.println("Compass Value of X,Y,Z:");
+  Serial.print(Mxyz[0]);
+  Serial.print(",");
+  Serial.print(Mxyz[1]);
+  Serial.print(",");
+  Serial.println(Mxyz[2]);
+  Serial.println("The clockwise angle between the magnetic north and X-Axis:");
+  Serial.print(heading);
+  Serial.println(" ");
+  Serial.println("The clockwise angle between the magnetic north and the projection of the positive X-Axis in the horizontal plane:");
+  Serial.println(tiltheading);
+  Serial.println("   ");
+  Serial.println("   ");
+  Serial.println("   ");
+  float tit1 = tiltheading;
+  delay(300);
+  getAccel_Data();
+  getGyro_Data();
+  getCompassDate_calibrated(); // compass data has been calibrated here
+  getHeading();       //before we use this function we should run 'getCompassDate_calibrated()' frist, so that we can get calibrated data ,then we can get correct angle .
+  getTiltHeading();
+  float tit2 = tiltheading;
+  float titavg = tit1 - tit2;
+  Serial.println(tit1);
+  Serial.println(tit2);
+  Serial.println(titavg);
+  if (abs(titavg) >= 100) {
+    Serial.println("****************Fall!***************");
+    Serial.print("連線至");
+    //
+    while (!Serial) {
+    }
+    while (status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to SSID: ");
+      Serial.println(ssid);
+      status = WiFi.begin(ssid, pass);
+    }
+    Serial.println("Connected to wifi");
+    printWifiStatus();
+    Serial.println("\nStarting connection to server...");
+    if (client.connect(server, 80)) {
+      Serial.println("connected to server (GET)");
+      client.println("GET /trigger/danger/with/key/dz54yhpbQe0DR4vw21Skz4 HTTP/1.0");
+      client.println("Host: maker.ifttt.com");
+      client.println("Accept: */*");
+      client.println("Connection: close");
+      client.println();
+      delay(10);
+    }
+    while (client.available()) {
+      char c = client.read();
+      Serial.write(c);
+    }
+    if (!client.connected()) {
+      Serial.println();
+      Serial.println("disconnecting from server.");
+      client.stop();
+      while (true);
+    }
+  }
+}
+void getHeading(void)
+{
+  heading = 180 * atan2(Mxyz[1], Mxyz[0]) / PI;
+  if (heading < 0) heading += 360;
+}
+void getTiltHeading(void)
+{
+  float pitch = asin(-Axyz[0]);
+  float roll = asin(Axyz[1] / cos(pitch));
+
+  float xh = Mxyz[0] * cos(pitch) + Mxyz[2] * sin(pitch);
+  float yh = Mxyz[0] * sin(roll) * sin(pitch) + Mxyz[1] * cos(roll) - Mxyz[2] * sin(roll) * cos(pitch);
+  float zh = -Mxyz[0] * cos(roll) * sin(pitch) + Mxyz[1] * sin(roll) + Mxyz[2] * cos(roll) * cos(pitch);
+  tiltheading = 180 * atan2(yh, xh) / PI;
+  if (yh < 0)    tiltheading += 360;
+}
+void Mxyz_init_calibrated ()
+{
+
+  Serial.println(F("Before using 9DOF,we need to calibrate the compass frist,It will takes about 2 minutes."));
+  Serial.print("  ");
+  Serial.println(F("During  calibratting ,you should rotate and turn the 9DOF all the time within 2 minutes."));
+  Serial.print("  ");
+  Serial.println(F("If you are ready ,please sent a command data 'ready' to start sample and calibrate."));
+  while (!Serial.find("ready"));
+  Serial.println("  ");
+  Serial.println("ready");
+  Serial.println("Sample starting......");
+  Serial.println("waiting ......");
+
+  get_calibration_Data ();
+
+  Serial.println("     ");
+  Serial.println("compass calibration parameter ");
+  Serial.print(mx_centre);
+  Serial.print("     ");
+  Serial.print(my_centre);
+  Serial.print("     ");
+  Serial.println(mz_centre);
+  Serial.println("    ");
+}
+void get_calibration_Data ()
+{
+  for (int i = 0; i < sample_num_mdate; i++)
+  {
+    get_one_sample_date_mxyz();
+    if (mx_sample[2] >= mx_sample[1])mx_sample[1] = mx_sample[2];
+    if (my_sample[2] >= my_sample[1])my_sample[1] = my_sample[2]; //find max value
+    if (mz_sample[2] >= mz_sample[1])mz_sample[1] = mz_sample[2];
+    if (mx_sample[2] <= mx_sample[0])mx_sample[0] = mx_sample[2];
+    if (my_sample[2] <= my_sample[0])my_sample[0] = my_sample[2]; //find min value
+    if (mz_sample[2] <= mz_sample[0])mz_sample[0] = mz_sample[2];
+  }
+  mx_max = mx_sample[1];
+  my_max = my_sample[1];
+  mz_max = mz_sample[1];
+  mx_min = mx_sample[0];
+  my_min = my_sample[0];
+  mz_min = mz_sample[0];
+  mx_centre = (mx_max + mx_min) / 2;
+  my_centre = (my_max + my_min) / 2;
+  mz_centre = (mz_max + mz_min) / 2;
+
+}
+
+void get_one_sample_date_mxyz()
+{
+  getCompass_Data();
+  mx_sample[2] = Mxyz[0];
+  my_sample[2] = Mxyz[1];
+  mz_sample[2] = Mxyz[2];
+}
+
+
+void getAccel_Data(void)
+{
+  accelgyro.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
+  Axyz[0] = (double) ax / 16384;//16384  LSB/g
+  Axyz[1] = (double) ay / 16384;
+  Axyz[2] = (double) az / 16384;
+}
+
+void getGyro_Data(void)
+{
+  accelgyro.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
+  Gxyz[0] = (double) gx * 250 / 32768;//131 LSB(��/s)
+  Gxyz[1] = (double) gy * 250 / 32768;
+  Gxyz[2] = (double) gz * 250 / 32768;
+}
+
+void getCompass_Data(void)
+{
+  I2C_M.writeByte(MPU9150_RA_MAG_ADDRESS, 0x0A, 0x01); //enable the magnetometer
+  delay(10);
+  I2C_M.readBytes(MPU9150_RA_MAG_ADDRESS, MPU9150_RA_MAG_XOUT_L, 6, buffer_m);
+
+  mx = ((int16_t)(buffer_m[1]) << 8) | buffer_m[0] ;
+  my = ((int16_t)(buffer_m[3]) << 8) | buffer_m[2] ;
+  mz = ((int16_t)(buffer_m[5]) << 8) | buffer_m[4] ;
+
+  //Mxyz[0] = (double) mx * 1200 / 4096;
+  //Mxyz[1] = (double) my * 1200 / 4096;
+  //Mxyz[2] = (double) mz * 1200 / 4096;
+  Mxyz[0] = (double) mx * 4800 / 8192;
+  Mxyz[1] = (double) my * 4800 / 8192;
+  Mxyz[2] = (double) mz * 4800 / 8192;
+}
+
+void getCompassDate_calibrated ()
+{
+  getCompass_Data();
+  Mxyz[0] = Mxyz[0] - mx_centre;
+  Mxyz[1] = Mxyz[1] - my_centre;
+  Mxyz[2] = Mxyz[2] - mz_centre;
+}
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  Serial.print(bssid[5], HEX);
+  Serial.print(":");
+  Serial.print(bssid[4], HEX);
+  Serial.print(":");
+  Serial.print(bssid[3], HEX);
+  Serial.print(":");
+  Serial.print(bssid[2], HEX);
+  Serial.print(":");
+  Serial.print(bssid[1], HEX);
+  Serial.print(":");
+  Serial.println(bssid[0], HEX);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+  Serial.println();
+}
+void printWifiData() {
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.println(ip);
+
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  Serial.print(mac[5], HEX);
+  Serial.print(":");
+  Serial.print(mac[4], HEX);
+  Serial.print(":");
+  Serial.print(mac[3], HEX);
+  Serial.print(":");
+  Serial.print(mac[2], HEX);
+  Serial.print(":");
+  Serial.print(mac[1], HEX);
+  Serial.print(":");
+  Serial.println(mac[0], HEX);
+
+}
+void printWifiStatus() {
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
